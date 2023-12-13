7cbaafc487730fe5a3102918a3e3096d1d20e9a6
Hamzata Fofana <hfofana@vermeer.com>
2023-12-13 14:22:02 -0600

    End of section14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -7,6 +7,7 @@ import { PaginatedResult } from '../_modules/pagination';
 import { UserParams } from '../_models/userParams';
 import { AccountService } from './account.service';
 import { User } from '../_models/user';
+import { PAGER_CONTROL_VALUE_ACCESSOR } from 'ngx-bootstrap/pagination/pager.component';
 
 @Injectable({
   providedIn: 'root'
@@ -89,6 +90,16 @@ export class MembersService {
 
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
+  addLike(username: string){
+    return this.http.post(this.baseUrl+ 'likes/'+ username, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize:number){
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate',predicate);
+    return this.http.get<Member[]>(this.baseUrl+'likes?predicate'+ predicate);
   }
 
   private getPaginatedResult<T>(url: string, params: HttpParams) {
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MembersService } from '../_services/members.service';
+import { Member } from '../_models/member';
+import { Pagination } from '../_modules/pagination';
 
 @Component({
   selector: 'app-lists',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent {
+  members: Member[]|undefined;
+  predicate = 'liked';
+  pageNumber = 1;
+  PageSize =5;
+  pagination: Pagination |undefined
 
+  constructor(private memberService: MembersService) {}
+  ngOnInit():void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.memberService.getLikes(this.predicate, this.pageNumber, this.PageSize).subscribe({
+      next: response =>{
+      this.members = response
+      }
+    })
+  }
+
+  pageChanged(event:any){
+    if(this.pageNumber!==event.page){
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
 }
